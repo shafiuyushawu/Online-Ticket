d956e4258e3051a0815ed886df2ff6f278817473
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-02-10 21:31:34 +0000

    Fixed linter error

--- docs/Navbar.js
+++ docs/Navbar.js
@@ -1,13 +1,13 @@
 function Menu(e) {
-  const menuList = document.querySelector("#nav_ul");
-  if (e.name === "menu") {
-    e.name = "close";
-    menuList.classList.add("top-[80px]");
-    menuList.classList.add("opacity-100");
+  const menuList = document.querySelector('#nav_ul');
+  if (e.name === 'menu') {
+    e.name = 'close';
+    menuList.classList.add('top-[80px]');
+    menuList.classList.add('opacity-100');
   } else {
-    e.name = "menu";
-    menuList.classList.remove("top-[80px]");
-    menuList.classList.remove("opacity-100");
+    e.name = 'menu';
+    menuList.classList.remove('top-[80px]');
+    menuList.classList.remove('opacity-100');
   }
 }
 
